0803e8db351f8e1d062a26680c7bbb03da81d032
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 02:33:55 +0600

    [ADDED] demo context added and showed in front end

--- restaurant_kitchen_app/urls.py
+++ restaurant_kitchen_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='index'),
-    path('/menu/<int:menu_id>/', views.MenuDetail.as_view(), name='menu_details'),
+    path('menu/<int:menu_id>/', views.MenuDetail.as_view(), name='menu_details'),
 ]
--- restaurant_kitchen_app/views.py
+++ restaurant_kitchen_app/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Menu
@@ -5,6 +6,17 @@ from .models import Menu
 class MenuList(generic.ListView):
     queryset = Menu.objects.order_by('-date_created')
     template_name = 'index.html'
+
+    def get_context_data(self):
+        context = {
+            'Dinner': 'Dinner',
+            'Lunch': 'Lunch',
+            'Breakfast': 'Breakfast',
+            'Dessert': 'Dessert',
+            'Drinks': 'Drinks',
+            'Salads': 'Salads',
+        }
+        return context
 
 
 class MenuDetail(generic.DetailView):
