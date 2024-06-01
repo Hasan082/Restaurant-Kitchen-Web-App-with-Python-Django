1eeaf72661f0abf6d09d07b89941eea52b5fdbbc
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 02:17:35 +0600

    [ADDED] Urls added and template connected and load the site

---
+++ restaurant_kitchen_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.MenuList.as_view(), name='index'),
+    path('/menu/<int:menu_id>/', views.MenuDetail.as_view(), name='menu_details'),
+]
