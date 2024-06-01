41f5c974da0057019926ce6dd123aa2a868899a7
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 01:56:37 +0600

    [CREATED] Views and html file cerated

--- restaurant_kitchen_app/views.py
+++ restaurant_kitchen_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Menu
 
-# Create your views here.
+class MenuList(generic.ListView):
+    queryset = Menu.objects.order_by('-date_created')
+    template_name = 'index.html'
+    pass
+
+
+class MenuDetail(generic.DetailView):
+    model = Menu
+    template_name = 'menu_details.html'
