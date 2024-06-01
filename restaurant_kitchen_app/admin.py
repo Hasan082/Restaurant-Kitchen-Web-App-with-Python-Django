9182a1f1c5b680d7e878e2c85ccfe22ea213c835
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 02:52:28 +0600

    [CREATED Connect admin and create a super user

--- restaurant_kitchen_app/admin.py
+++ restaurant_kitchen_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Menu
 
-# Register your models here.
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('meal','description','price', 'meal_type', 'status')
+    list_filter = ('meal_type', 'status')
+    search_fields = ('meal', 'description')
+
+admin.site.register(Menu, MenuAdmin)
