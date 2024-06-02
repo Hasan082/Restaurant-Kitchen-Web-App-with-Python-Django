d8f6a782086d58288bbbdc27f568f481ec804d07
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 08:27:53 +0600

    [ADDED] Menu detals page make dynamic

--- restaurant_kitchen_app/urls.py
+++ restaurant_kitchen_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='index'),
-    path('menu/<int:menu_id>/', views.MenuDetail.as_view(), name='menu_details'),
+    path('menu/<int:pk>/', views.MenuDetail.as_view(), name='menu_details'),
+    # pk = primary key
 ]
