9c3ce24cf85e77d65c2521920642e8f85f6d2973
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 14:21:59 +0600

    [UPDATED] views and index file updated

--- restaurant_kitchen_app/views.py
+++ restaurant_kitchen_app/views.py
@@ -11,6 +11,7 @@ class MenuList(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['meals'] = meal_types
+        context['menus'] = Menu.objects.all()
         return context
 
 
